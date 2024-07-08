e69ddaed8f6b885a6c1ac844f53daadc8b11fd80
m3rri <m3rri17@gmail.com>
2024-07-08 15:57:10 +0900

    ga 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,56 @@
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import Layout from 'component/Organisms/Layout';
-import 'styles/globals.css';
-import 'github-markdown-css';
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Layout from "component/Organisms/Layout";
+import "styles/globals.css";
+import "github-markdown-css";
+import * as gtag from "libs/gtags";
+import Script from "next/script";
+import { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const {asPath} = router;
-  
-  return <Layout asPath={asPath}>
-    <Component {...pageProps} />
-  </Layout>;
+    const router = useRouter();
+    const { asPath } = router;
+    useEffect(() => {
+        const handleRouteChange = (url: URL) => {
+            gtag.pageview(url);
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
+
+    return (
+        <>
+            <Head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${gtag.GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                        `,
+                    }}
+                />
+            </Head>
+            {process.env.NODE_ENV !== "development" && (
+                <>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <Script
+                        strategy="afterInteractive"
+                        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+                    />
+                </>
+            )}
+            <Layout asPath={asPath}>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
 
 export default MyApp;
