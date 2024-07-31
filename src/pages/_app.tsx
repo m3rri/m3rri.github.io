c4b342bafed8edea2883828bbd72e978a69bef59
m3rri <m3rri17@gmail.com>
2024-07-31 10:54:55 +0900

    google search meta tag 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,6 +44,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                         strategy="afterInteractive"
                         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
                     />
+                    <meta name="google-site-verification" content="-IYFAYTqi7mYYCPb6BhJKCcZVcWghBEgTqFyxyXrNBc" />
                 </>
             )}
             <Layout asPath={asPath}>
